c19ba0aa757f8e13265ad40f53fa75e5ca2820fb
Rahul06x1 <118870154+Rahul06x1@users.noreply.github.com>
2022-11-24 18:10:35 +0530

    Add files via upload

--- VehicleRental/models/vehicle_rental_property.py
+++ VehicleRental/models/vehicle_rental_property.py
@@ -4,16 +4,15 @@ from odoo import fields, models
 class VehicleRentalModel(models.Model):
     _name = "vehicle.rental.property"
     _description = "vehicle rental model"
-
-    vehicle = fields.Many2one('fleet.vehicle', 'Vehicle',domain = [("state_id.name", "=", "Registered")])
+    customer_name = fields.Many2one('res.partner', 'Customer')
+    vehicle = fields.Many2one('fleet.vehicle', 'Vehicle', domain=[("state_id.name", "=", "Registered")])
 
     # vehicle_name = fields.One2many('fleet.vehicle','brand_id','Vname')
-    brand_name = fields.Char('Brand', required=True, help="Brand of the vehicle", related='vehicle.brand_id.name')
-    registeration_date = fields.Date('regesteration date',related='vehicle.acquisition_date')
-    model_year = fields.Char("model year",related='vehicle.model_year')
+    brand_name = fields.Char('Brand', help="Brand of the vehicle", related='vehicle.brand_id.name',store=True) # required=True,
+    registeration_date = fields.Date('registration date', related='vehicle.acquisition_date')
+    model_year = fields.Char("model year", related='vehicle.model_year',readonly=0)
 
-
-    # vehicle_rent =
+    vehicle_rent = fields.Integer("Rent")
     state = fields.Selection(
         selection=[
             ('available', 'Available'),
@@ -22,4 +21,4 @@ class VehicleRentalModel(models.Model):
 
         ], default='available'
     )
-   
+# customer_name.
