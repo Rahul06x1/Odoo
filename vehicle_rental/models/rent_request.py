66908014ec12264ff0bef04947fa8b6993cbe215
cybrosys <rahulrajeev2k1@gmail.com>
2022-11-28 18:02:10 +0530

    [UPDT] vehicle rental2 90% completed

--- vehicle_rental/models/rent_request.py
+++ vehicle_rental/models/rent_request.py
@@ -1,17 +1,56 @@
 from odoo import fields, models, api
 from datetime import datetime
+from odoo.exceptions import ValidationError
 
 
 class RentalRequest(models.Model):
     _name = "rent.request"
     _description = "rental request"
+    _rec_name = "sequence"
+
+    sequence = fields.Char("Rental Order No", default="new")
+    # sequence = fields.Char(compute='_vehicle_name_modelyear')
 
     customer_name = fields.Many2one('res.partner', 'Customer')
     request_date = fields.Date('request date', default=datetime.today())
-    vehicle = fields.Many2one('fleet.vehicle', 'Vehicle')
+    vehicle = fields.Many2one('vehicle.rental.property', 'Vehicle')
     from_date = fields.Date('from date')
     to_date = fields.Date('to date')
-    period = fields.Integer('Period')
+    period = fields.Integer('Period', readonly=1)
+    model_year = fields.Char("model year", related='vehicle.model_year', readonly=0)
+    company_id = fields.Many2one('res.company', 'Company')
+    currency_id = fields.Many2one('res.currency', 'Currency',
+                                  default=lambda self: self.env.company.currency_id.id, required=True)
+    vehicle_rent = fields.Integer('Rent',readonly=1)
+    # vehicle_rent = fields.Many2one('vehicle.rental.property.vehicle.rent', 'Rent')
+    request_state = fields.Selection(
+        selection=[
+            ('draft', 'Draft'),
+            ('confirmed', 'Confirmed'),
+            ('returned', 'Returned'),
+
+        ], default='draft'
+    )
+
+
+    @api.onchange('vehicle')
+    def _onchange_type(self):
+        # print(self.vehicle.vehicle_rent)
+        self.vehicle_rent = self.vehicle.vehicle_rent
+
+    # model_year = fields.Many2one('vehicle.rental.property.model.year', 'Vehicle')
+    @api.model
+    def create(self, vals):
+        vals['sequence'] = self.env['ir.sequence'].next_by_code('rent.request')
+        return super(RentalRequest, self).create(vals)
+
+    def action_confirm(self):
+        self.request_state = "confirmed"
+        self.vehicle.state = "not_available"
+
+    def action_return(self):
+        self.request_state = "returned"
+        self.vehicle.state = "available"
 
     @api.onchange('from_date', 'to_date')
     def calculate_date(self):
@@ -21,12 +60,8 @@ class RentalRequest(models.Model):
             d3 = d2 - d1
             self.period = str(d3.days)
 
-    request_state = fields.Selection(
-        selection=[
-            ('draft', 'Draft'),
-            ('confirmed', 'Confirmed'),
-            ('returned', 'Returned'),
-
-        ], default='draft'
-    )
-    rent = fields.Integer('Rent')
+    @api.constrains('to_date', 'from_date')
+    def date_constrains(self):
+        for rec in self:
+            if rec.to_date < rec.from_date:
+                raise ValidationError('Sorry, End Date Must be greater Than Start Date...')
--- vehicle_rental/models/vehicle_rental_property.py
+++ vehicle_rental/models/vehicle_rental_property.py
@@ -1,24 +1,38 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from datetime import datetime
 
 
 class InheritFleet(models.Model):
     _inherit = 'fleet.vehicle'
     registration_date = fields.Date("Registration Date")
+    # model_year=fields.Char("Model Year")
+
+    # model_year = registration_date.year
 
 
 class VehicleRentalModel(models.Model):
     _name = "vehicle.rental.property"
-    _rec_name = "customer_name"
+    _rec_name = "combination"
     _description = "vehicle rental model"
     _inherit = 'mail.thread', 'mail.activity.mixin'
 
-    customer_name = fields.Many2one('res.partner', 'Customer')
-    vehicle = fields.Many2one('fleet.vehicle', 'Vehicle', domain=[("state_id.name", "=", "Registered")])
+    # customer_name = fields.Many2one('res.partner', 'Customer')
+
+    vehicle = fields.Many2one('fleet.vehicle', 'Vehicle', required=True, domain=[("state_id.name", "=", "Registered")])
 
     brand_name = fields.Char('Brand', help="Brand of the vehicle", related='vehicle.brand_id.name',
                              store=True)  # required=True,
     registration_date = fields.Date('registration date', related='vehicle.registration_date', readonly=0)
+    # model_year = registration_date.year
     model_year = fields.Char("model year", related='vehicle.model_year', readonly=0)
+
+    @api.onchange('vehicle.registration_date')
+    def _onchange_type(self):
+        if self.vehicle.registration_date:
+            print(self.vehicle.registration_date.year)
+            self.model_year = self.vehicle.registration_date.year
+        print(self.model_year)
+            # self.fleet.vehicle.model_year = self.fleet.vehicle.registration_date.year
 
     company_id = fields.Many2one('res.company', 'Company')
     currency_id = fields.Many2one('res.currency', 'Currency',
@@ -33,3 +47,10 @@ class VehicleRentalModel(models.Model):
 
         ], default='available'
     )
+    combination = fields.Char(string='Combination', compute='_compute_fields_combination')
+
+    @api.depends('vehicle.display_name', 'model_year')
+    def _compute_fields_combination(self):
+        for test in self:
+            test.combination = test.vehicle.display_name
+            # + '/' + test.vehicle.model_year)
