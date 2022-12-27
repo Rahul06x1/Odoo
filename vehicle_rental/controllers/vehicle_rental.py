9efdc876e9cf85e9d692eb9c694513a6ea35f8b9
cybrosys <rahulrajeev2k1@gmail.com>
2022-12-27 17:21:18 +0530

    [UPDT]

--- vehicle_rental/controllers/vehicle_rental.py
+++ vehicle_rental/controllers/vehicle_rental.py
@@ -7,17 +7,37 @@ class VehicleRental(http.Controller):
 
     @http.route('/vehicle_rental_web_form/', type="http", website=True, auth='public')
     def vehicle_rental_web_form(self, **kw):
-        customer_rec = request.env['res.partner'].sudo().search([])
+        # customer_rec = request.env['res.partner'].sudo().search([])
         vehicle_rec = request.env['vehicle.rental.property'].sudo().search([])
         period_type_rec = request.env['time.selection'].sudo().search([])
 
         return http.request.render("vehicle_rental.vehicle_rental_page", {
-            'customer_rec': customer_rec,
+            # 'customer_rec': customer_rec,
+            'customer_rec': request.env.user.partner_id,
             'vehicle_rec': vehicle_rec,
             'period_type_rec': period_type_rec
         })
 
     @http.route('/create_rent_request/', type="http", website=True, auth='public')
     def create_rent_request(self, **kw):
+        print(kw)
+        partner = request.env['res.partner'].sudo().search([('name', '=', kw['customer_name'])], limit=1)
+        print(partner)
+        # if user:
+        #     partner = user.partner_id.id
+        #
+        # # user_id = request.env['res.user'].browse(int(request.env['res.user']))
+        #     kw['customer_name'] = partner
+        if not partner:
+            partner = request.env['res.partner'].sudo().create({
+                'name': kw['customer_name']
+            })
+
+        # partner = partner.id
+
+        # user_id = request.env['res.user'].browse(int(request.env['res.user']))
+        kw['customer_name'] = partner.id
+
+        print(kw)
         request.env['rent.request'].sudo().create(kw)
         return request.render("vehicle_rental.rent_request_thanks", {})
--- vehicle_rental/static/src/js/vehicle_rental_website.js
+++ vehicle_rental/static/src/js/vehicle_rental_website.js
@@ -4,6 +4,7 @@ odoo.define('vehicle_rental.period_type', function (require) {
 var domain = [];
 // Use an empty array to read all the fields of the records
 var fields = [];
+
 var rpc = require('web.rpc');
 
     rpc.query({
@@ -11,65 +12,35 @@ var rpc = require('web.rpc');
     method: 'search_read',
     args: [domain, fields],
 }).then(function (data) {
-$("#period_type").change(function(){
-//    alert("sssssssssssrrrrrrrrrrrrr");
-//    $('#time_amount').val($(this).val())
-//    console.log(data,'dataaaaaaaaaaaaaaa');
-    var current = data.filter((elem) => elem.id == $(this).val())
-    $('#time_amount').val(current[0].time_amount)
-//    console.log('ssssssssss', current)
-    });
-    });
-    });
-//    =================================================================================================================
-//    rpc.query({
-//    model: 'vehicle.rental.property',
-//    method: 'search_read',
-//    args: [domain, fields],
-//}).then(function (data) {
-//$("#vehicle").change(function(){
-////    alert("sssssssssssrrrrrrrrrrrrr");
-////    $('#time_amount').val($(this).val())
-//    console.log(data,'dataaaaaaaaaaaaaaa');
-//    var v_current = data.filter((elem) => elem.id == $(this).val())
-////    $('#time_amount').val(current[0].time_amount)
-//    c
-//    console.log('ssssssssss', v_current)
-//    });
-//    });
-//    });
-//    $.each(data,function(index,value){
-//    console.log(index);
-//    console.log(value);
-//    if ($(this).val() == index){
-//    alert('asasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas');
-//    console.log('sssssssssssssaaaaaaaaa');
-//    }
-//
-////    console.log(index);
-////    console.log(value);
-//   console.log(index,  value);
-//   console.log(index,  value.time_amount,'gg');
-//   console.log(data.time_amount,'sssssssssssss');
-//    });
+$("#vehicle").change(function(){
+//if ($('#vehicle').find(":selected").val()!='default'){
 
-//    $('#time_amount').val(89)
-//    console.log(data);
+                    var current = data.filter((elem) => elem.relation_id[0] == $(this).val())
 
-//    });
-//    });
-//    });
-//    alert("sssssssssssrrrrrrrrrrrrr");
-//    $.each(data,function(index,value){
-//   console.log(index,  value);
-//   console.log(index,  value[0],'gg');
-//});
-//
-//
-//});
-////        alert("sssssssssssrrrrrrrrrrrrr");
-//      });
-//      });
-//
-//
-//
+                    $('#period_type').find('option').remove().end();
+                    $('#time_amount').val(' ');
+                    $.each(current, function(index) {
+                    console.log(current);
+                        $('#period_type').append('<option value='+current[index].id+' data-time_amount='+current[index].time_amount+'>'+current[index].selection_time+ '</option>');
+                        console.log($('#period_type'),'sssssssssssaaaaa')
+
+                            });
+//                            }
+        });
+    });
+
+    rpc.query({
+                model: 'time.selection',
+                method: 'search_read',
+                args: [domain, fields],
+                }).then(function (data) {
+                $("#period_type").change(function(){
+                console.log($('#period_type').find(":selected"));
+            //    var current = data.filter((elem) => elem.id == $(this).val())
+                $('#time_amount').val($('#period_type').find(":selected").data('time_amount'));
+            });
+    });
+    });
+
+
+
